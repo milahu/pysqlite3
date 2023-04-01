fb0374a5420371feda7324ec831240bfaf801e5f
Milan Hauth <milahu@gmail.com>
2023-04-01 16:21:05 +0200

    generate.sh: pretty

--- src/pysqlite3/parser/generate.sh
+++ src/pysqlite3/parser/generate.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
+
 set -e
+
 ksc=$KSC # use ksc from env
 [ -z "$ksc" ] && ksc=$(which kaitai-struct-compiler 2>/dev/null)
+
 set -x
+
 cd "$(dirname "$0")"
+
+# generate sqlite3.py and vlq_base128_be.py
 $ksc -t python --python-package . sqlite3.ksy
+
+# reformat python files
 black .
