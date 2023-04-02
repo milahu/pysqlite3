114d3c697b8746f701b9103348a0ff960f702b40
Milan Hauth <milahu@gmail.com>
2023-04-02 16:17:10 +0200

    copy sqlite3.ksy

--- src/pysqlite3/parser/generate.sh
+++ src/pysqlite3/parser/generate.sh
@@ -9,6 +9,8 @@ set -x
 
 cd "$(dirname "$0")"
 
+cp ../../../kaitai_struct_formats/database/sqlite3.ksy sqlite3.ksy || true
+
 # generate sqlite3.py and vlq_base128_be.py
 $ksc -t python --python-package . sqlite3.ksy
 
